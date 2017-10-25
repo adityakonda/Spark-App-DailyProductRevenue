6c35c11f6e9b65d877dd6435e516e8f1244286b2
kondaaditya <adityaprasad.k@gmail.com>
2017-10-25 17:11:26 -0400

    Spark Reading Data from RDBMS
    reading data from a MySql

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "2.2.0"
 
+// https://mvnrepository.com/artifact/mysql/mysql-connector-java
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.6"
--- src/main/scala/com/sparkapplications/JdbcRdd.scala
+++ src/main/scala/com/sparkapplications/JdbcRdd.scala
@@ -1,5 +1,11 @@
 package com.sparkapplications
 
+import java.sql.DriverManager
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.JdbcRDD
+import com.mysql.jdbc.Driver
+
 /**
   * Created by adity on 10/25/2017.
   */
@@ -7,10 +13,24 @@ object JdbcRdd {
 
   def main(args: Array[String]): Unit = {
 
-    //JDBC connection
-    val url = "jdbc:mysql://localhost:3306/sakila?profileSQL=true"
+    //JDBC connection & it's crenditials
+    val url = "jdbc:mysql://localhost:3306/sakila"
+    val username = "root"
+    val password = "password"
 
+    //JDBC driver connection
+    Class.forName("com.mysql.jdbc.Driver").newInstance()
 
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("JDBC Rdd")
+    val sc = new SparkContext(conf)
+
+    val myRdd = new JdbcRDD(sc, () => DriverManager.getConnection(url,username,password),
+      "SELECT * FROM sakila.actor limit ?,?",10,20,1,
+      r => r.getString("first_name") + "," +r.getString("last_name"))
+
+    myRdd.foreach(println)
 
   }
 
